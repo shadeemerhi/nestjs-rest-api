d17d89254e5444182fa4113addfecb95ae8e86ea
Shadee Merhi <shadmerhi@gmail.com>
2021-10-08 15:46:34 -0600

    create item service, attach to module

---
+++ src/items/items.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
+
+@Injectable()
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '3343434453213',
+      name: 'Item One',
+      qty: 100,
+      description: 'This is item one',
+    },
+    {
+      id: '239573201297',
+      name: 'Item Two',
+      qty: 159,
+      description: 'This is item two',
+    },
+  ];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+}
